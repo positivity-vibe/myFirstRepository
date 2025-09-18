43f57f4fdefd6849bb9340d454ea39b5c62271ac
Ivan <proterror88@gmail.com>
2025-09-18 16:05:04 +0500

    Test

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace myFirstRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int [number];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"Введите [{i}] элемент массива:  ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < myArray.Length; i++) {
+                Console.WriteLine($"myArray[{i}] = {myArray[i]}");
+            }
+        }
+    }
+}
