5b77a737cf06a7c77bc82bcffe65cd27f6ee39fe
Ivan <proterror88@gmail.com>
2025-09-19 09:26:59 +0500

    All assignments for the lesson

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace myFirstRepository
             Console.Write("Введите длину массива: ");
             int number = int.Parse(Console.ReadLine());
 
-            int[] myArray = new int [number];
+            int[] myArray = new int[number];
 
             for (int i = 0; i < myArray.Length; i++)
             {
@@ -18,11 +18,37 @@ namespace myFirstRepository
                 myArray[i] = int.Parse(Console.ReadLine());
             }
 
-            for (int i = myArray.Length - 1; i >= 0; i--)
+            for (int i = 0; i < myArray.Length; i++)
             {
                 Console.WriteLine($"myArray[{i}] = {myArray[i]}");
-                
             }
+
+            System.Console.WriteLine();
+            for (int i = myArray.Length - 1; i >= 0; i--)
+            {
+                System.Console.WriteLine($"myArray[{i}] = {myArray[i]}");
+            }
+
+                int sum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    sum += myArray[i];
+                }
+            }
+            System.Console.WriteLine($"\nСумма четных чисел массива = {sum}");
+
+
+            int min = myArray[0];
+            for (int i = 1; i < myArray.Length; i++)
+            {
+                if (myArray[i] < min)
+                {
+                    min = myArray[i];
+                }
+            }
+            System.Console.WriteLine($"\nНаименьший элемент массива: {min}");
         }
     }
 }
